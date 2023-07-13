5825661d3b82a3891e8d09d32770568a2b176136
lucas <lucas.lvn97439@gmail.com>
2023-07-13 09:33:50 +0400

    update

--- main.py
+++ main.py
@@ -4,47 +4,69 @@ from database import query
 import streamlit as st
 import pandas as pd
 
-st.title('Département & coût')
-department, numberEmployee, cost = query.getEmployeePerDepartmentAndTheyCost()
-data = {"Departement": department, "Nombre employer": numberEmployee, "Coût": cost}
-st.table(pd.DataFrame(data=data))
 
-st.title('Graphiques')
 
-tab1, tab2 = st.tabs(["Ventes par régions", "📈 Chiffre d'affaire par années"])
+def showDepartmentsAndCost():
+    st.title('Département & coût')
+    department, numberEmployee, cost = query.getEmployeePerDepartmentAndTheyCost()
+    data = {"Departement": department, "Nombre employer": numberEmployee, "Coût": cost}
+    st.table(pd.DataFrame(data=data))
 
-countries, sales = query.getSalesPerCountry()
-SalesPerCountry = [go.Choropleth(
-    locations = countries,
-    z = sales,
-    locationmode = 'country names',
-    colorscale = 'Blues',
-    colorbar_title = 'Sales'
-)]
+def showSalesPerCountry(tab):
+    countries, sales = query.getSalesPerCountry()
+    SalesPerCountry = [go.Choropleth(
+        locations = countries,
+        z = sales,
+        locationmode = 'country names',
+        colorscale = 'Blues',
+        colorbar_title = 'Sales'
+    )]
 
-layout = go.Layout(
-    title = 'Sales by Country',
-    geo = dict(
-        showframe = False,
-        projection = {'type':'natural earth'}
+    layout = go.Layout(
+        title = 'Sales by Country',
+        geo = dict(
+            showframe = False,
+            projection = {'type':'natural earth'}
+        )
     )
-)
 
-SalesPerCountriesFig = go.Figure(data=SalesPerCountry, layout=layout)
-tab1.plotly_chart(SalesPerCountriesFig)
+    SalesPerCountriesFig = go.Figure(data=SalesPerCountry, layout=layout)
+    tab.plotly_chart(SalesPerCountriesFig)
 
+def showCAPerYear(tab):
+    years, cas = query.getCAPerYear() 
+    TurnOverFig = px.bar(y=cas, x=years)
+    tab.plotly_chart(TurnOverFig)
 
-years, cas = query.getCAPerYear() 
+def showPieCasCountries(tab):
+    countries, cas = query.getCAsCountries()
+    countriesCASDataframe = pd.DataFrame({
+        "countries": countries,
+        "chiffre affaire": cas
+    })
+    fig = px.pie(countriesCASDataframe, values="chiffre affaire", names="countries")
+    tab.plotly_chart(fig)
 
-TurnOverFig = px.bar(y=cas, x=years)
-tab2.plotly_chart(TurnOverFig)
+# resellersKey, resellersCas = query.getResselersCA()
 
-sellersKey, sellerCas = query.getResselersCA()
+# resellersCas = pd.DataFrame({
+#     "Revendeur": resellersKey,
+#     "Chiffre d'affaire": resellersCas
+# })
 
-resellersCas = pd.DataFrame({
-    "Revendeur": sellersKey,
-    "Chiffre d'affaire": sellerCas
-})
+# fig = px.pie(resellersCas, values="Chiffre d'affaire", names="Revendeur")
+# st.plotly_chart(fig)
 
-fig = px.pie(resellersCas, values="Chiffre d'affaire", names="Revendeur")
-st.plotly_chart(fig)
+def showTabs():
+    st.title('Graphiques')
+
+    tab1, tab2, tab3 = st.tabs(["Ventes par régions", 
+                                "📈 Chiffre d'affaire par années", 
+                                "Chiffre d'affaire par pei"])
+                                
+    showSalesPerCountry(tab1)
+    showCAPerYear(tab2)
+    showPieCasCountries(tab3)
+
+showDepartmentsAndCost()
+showTabs()
